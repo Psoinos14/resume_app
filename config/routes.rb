61a95ec26e474fc73f9656568b262398f600e609
“Rishi <YOUR@EMAIL.com>
2019-08-19 17:55:48 -0700

    created experience controller and model with 5 restful actions

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   namespace :api do
 
     get '/students' => 'students#index'
+    get '/experiences' => 'experiences#index'
+    get '/experiences/:id' => 'experiences#show'
+    post '/experiences' => 'experiences#create'
+    patch '/experiences/:id' => 'experiences#update'
+    delete '/experiences/:id' => 'experiences#destroy'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_213223) do
+ActiveRecord::Schema.define(version: 2019_08_19_233555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experiences", force: :cascade do |t|
+    t.string "start_date"
+    t.string "end_date"
+    t.string "job_title"
+    t.string "company_name"
+    t.string "details"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "student_id"
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
