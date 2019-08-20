388572fd3e174c9d446cb47863dd7e1a90f6418b
Sim <simchasitzer1@gmail.com>
2019-08-19 21:18:12 -0400

    make education update and delete restful actions

--- app/controllers/api/educations_controller.rb
+++ app/controllers/api/educations_controller.rb
@@ -1,12 +1,12 @@
 class Api::EducationsController < ApplicationController
   def index
     @educations = Education.all
-    render "index.json.jb"
+    render  'index.json.jb'
   end
 
   def show
     @education = Education.find_by(id: params[:id])
-    render "show.json.jb"
+    render 'show.json.jb'
   end
 
   def create
@@ -17,7 +17,34 @@ class Api::EducationsController < ApplicationController
     @education.job_title = params[:job_title]
     @education.company_name = params[:company_name]
     @education.details = params[:details]
-    @education.save
-    render 'show.json.jb'
+    if @education.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @education.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    @education = Education.find_by(id: params[:id])
+    @education.student_id = params[:student_id] || @education.student_id
+    @education.start_date = params[:start_date] || @education.start_date
+    @education.end_date = params[:end_date] || @education.end_date
+    @education.job_title = params[:job_title] || @education.job_title
+    @education.company_name = params[:company_name] || @education.company_name
+    @education.details = params[:details] || @education.details
+    if @education.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @education.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    @education = Education.find_by(id: params[:id])
+    if @education.destroy
+      render json: {message: "Education Succesfully Deleted"}
+    else
+      render json: {errors: @education.errors.full_messages}, status: :unprocessible_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
     get '/educations' => 'educations#index'
     get '/educations/:id' => 'educations#show'
     post '/educations' => 'educations#create'
+    patch '/educations/:id' => 'educations#update'
+    delete '/educations/:id' => 'educations#destroy'
   end
 end
